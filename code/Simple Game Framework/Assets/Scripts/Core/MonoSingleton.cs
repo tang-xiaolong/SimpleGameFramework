cf80f61a06a40dc95922f5f9cf2f1d9d4372a77f
tang-xiaolong <1097145586@qq.com>
2021-02-22 01:45:25 +0800

    增加单例模块代码

---
+++ code/Simple Game Framework/Assets/Scripts/Core/MonoSingleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance = null;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name, new[] {typeof(T)});
+                    DontDestroyOnLoad(obj);
+                    _instance = obj.GetComponent<T>();
+                    (_instance as IInitable)?.Init();
+                }
+                else
+                {
+                    Debug.LogWarning("Instance is already exist!");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// 继承Mono单例的类如果写了Awake方法，需要在Awake方法最开始的地方调用一次base.Awake()，来给_instance赋值
+    /// </summary>
+    private void Awake()
+    {
+        _instance = this as T;
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ code/Simple Game Framework/Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,22 @@
+﻿public class Singleton<T> where T : new()
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new T();
+                (_instance as IInitable)?.Init();
+            }
+
+            return _instance;
+        }
+    }
+}
+
+public class ClassA : Singleton<ClassA>
+{
+}
