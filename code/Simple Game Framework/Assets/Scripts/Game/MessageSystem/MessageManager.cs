036b4f22bfab8ce3092a988b3a59fe7f60881f36
唐小龙 <1097145586@qq.com>
2021-03-06 20:22:36 +0800

    上传消息系统代码

---
+++ code/Simple Game Framework/Assets/Scripts/Game/MessageSystem/MessageManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MessageManager : Singleton<MessageManager>
+{
+    private Dictionary<string, IMessageData> dictionaryMessage;
+
+    public MessageManager()
+    {
+        InitData();
+    }
+
+    private void InitData()
+    {
+        dictionaryMessage = new Dictionary<string, IMessageData>();
+    }
+
+    public void Register<T>(string key, UnityAction<T> action)
+    {
+        if (dictionaryMessage.TryGetValue(key, out var previousAction))
+        {
+            if (previousAction is MessageData<T> messageData)
+            {
+                messageData.MessageEvents += action;
+            }
+        }
+        else
+        {
+            dictionaryMessage.Add(key, new MessageData<T>(action));
+        }
+    }
+
+    public void Remove<T>(string key, UnityAction<T> action)
+    {
+        if (dictionaryMessage.TryGetValue(key, out var previousAction))
+        {
+            if (previousAction is MessageData<T> messageData)
+            {
+                messageData.MessageEvents -= action;
+            }
+        }
+    }
+
+    public void Send<T>(string key, T data)
+    {
+        if (dictionaryMessage.TryGetValue(key, out var previousAction))
+        {
+            (previousAction as MessageData<T>)?.MessageEvents.Invoke(data);
+        }
+    }
+
+    public void Clear()
+    {
+        dictionaryMessage.Clear();
+    }
+}
---
+++ code/Simple Game Framework/Assets/Scripts/TestMessage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMessage : MonoBehaviour
+{
+    [SerializeField] private int loopCount = 100000;
+
+    void Start()
+    {
+        MessageManager.Instance.Register<int>(MessageDefine.TEST_MESSAGE, TestTMessage);
+    }
+
+    private void OnDestroy()
+    {
+        MessageManager.Instance.Remove<int>(MessageDefine.TEST_MESSAGE, TestTMessage);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            for (int i = 0; i < loopCount; i++)
+            {
+                MessageManager.Instance.Send<int>(MessageDefine.TEST_MESSAGE, 1);
+            }
+        }
+    }
+
+    void TestTMessage(int data)
+    {
+        int i = data + 1;
+    }
+}
