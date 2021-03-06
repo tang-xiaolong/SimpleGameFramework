1dc1f05ab98d031fff6196423671e1f3b77fb664
唐小龙 <1097145586@qq.com>
2021-03-06 20:22:36 +0800

    上传消息系统代码

---
+++ code/Simple Game Framework/Assets/Scripts/Game/MessageSystem/MessageData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Events;
+
+public interface IMessageData
+{
+}
+
+public class MessageData<T> : IMessageData
+{
+    public UnityAction<T> MessageEvents;
+
+    public MessageData(UnityAction<T> action)
+    {
+        MessageEvents += action;
+    }
+}
---
+++ code/Simple Game Framework/Assets/Scripts/Game/MessageSystem/MessageDefine.cs
@@ -0,0 +1,5 @@
+﻿public static class MessageDefine
+{
+    //Debug event
+    public static readonly string TEST_MESSAGE = "TEST_MESSAGE"; //这是测试消息Key
+}
