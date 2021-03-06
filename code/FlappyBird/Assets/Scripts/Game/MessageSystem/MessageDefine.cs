f2e2b78f09b4a660779f01b779865b6e46ad7b6d
tang-xiaolong <1097145586@qq.com>
2021-03-07 01:36:10 +0800

    引入消息系统解决FlappyBird耦合性过强的问题

---
+++ code/FlappyBird/Assets/Scripts/Game/MessageSystem/MessageDefine.cs
@@ -0,0 +1,11 @@
+﻿public static class MessageDefine
+{
+    //UI Event
+    public static readonly string ON_BEGIN_GAME_CLICK = "ON_BEGIN_GAME_CLICK"; //点击开始游戏
+
+
+    //Game Event
+    public static readonly string BEGIN_GAME = "BEGIN_GAME"; //开始游戏
+    public static readonly string BIRD_DIE = "BIRD_DIE"; //小鸟死亡
+    public static readonly string ADD_SCORE = "ADD_SCORE"; //分数增加
+}
