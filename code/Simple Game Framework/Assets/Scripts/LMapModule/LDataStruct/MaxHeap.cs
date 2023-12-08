ad680011f2841b63eea772e7b9b0c105bd0ed1a4
xiaolong.tang <1097145586@qq.com>
2023-12-08 12:41:08 +0800

    增加最小堆和最大堆的类，并提供无参构造方法，方便在对象池中使用

---
+++ code/Simple Game Framework/Assets/Scripts/LMapModule/LDataStruct/MaxHeap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LDataStruct
+{
+    public class MaxHeap<T> : Heap<T> where T : IComparable
+    {
+        public MaxHeap() : base(10, HeapType.MaxHeap)
+        {
+        }
+    }
+}
---
+++ code/Simple Game Framework/Assets/Scripts/LMapModule/LDataStruct/MinHeap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LDataStruct
+{
+    public class MinHeap<T> : Heap<T> where T : IComparable
+    {
+        public MinHeap() : base(10, HeapType.MinHeap)
+        {
+        }
+    }
+}
